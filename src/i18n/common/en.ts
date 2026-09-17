b374398bdd7f4ce71114562fa181c31a48f093db
Federico <f.bachis91@gmail.com>
2026-09-17 08:00:46 +0200

    Correct job title in English localization
    Updated job title in bio2 from 'Game-Based Interaction Systems' to 'Game-Based Interaction Technologies'.

--- src/i18n/common/en.ts
+++ src/i18n/common/en.ts
@@ -22,7 +22,7 @@ export const commonTextEn = {
     days: "Days",
     hours: "Hours",
     bio1: "Indie **Game Developer** and **Unity Developer** since 2017.",
-    bio2: "I specialize in **3D environment development**, human-robot interaction, and **gamification** for research applications. Currently, I work as a **Research Technologist** in Game-Based Interaction Systems at **CRS4**, bridging the gap between advanced research and **immersive user experiences**.",
+    bio2: "I specialize in **3D environment development**, human-robot interaction, and **gamification** for research applications. Currently, I work as a **Research Technologist** in Game-Based Interaction Technologies at **CRS4**, bridging the gap between advanced research and **immersive user experiences**.",
     bio3: "I hold a degree in **Computer Science** from the University of Cagliari, with expertise in **scientific writing** and **technical software architecture**.",
     uplink: "Company Uplink",
     view_page: "View Page"
